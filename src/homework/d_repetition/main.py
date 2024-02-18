b78c7492d1ecfc1c6a2a4bb0df5357b14a12695f
Timemachinetom <108629469+Timemachinetom@users.noreply.github.com>
2024-02-18 01:17:20 +0000

    Homework 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,57 @@
-#
+import repetition
+
+option = 0
+quit = False
+
+while quit != True:
+    option = input ("Homework 3 Menu\n1-Factorial\n2-Sum odd numbers\n3-Exit")
+    option = int(option)
+
+    while option == 1:
+        num = input ("Enter a whole number between 1 and 10")
+        num = int(num)
+        if num >=1 and num <= 10:
+            factorial = repetition.get_factorial(num)
+            print(factorial)
+            exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
+            exit = int(exit)
+            if exit == 1:
+                exit = 0
+                option = 0
+                quit = True
+            elif exit == 2:
+                exit = 0
+                option = 0
+        elif num <1 or num > 10:
+            print("(num) is not a valid number. Please enter a whole number between 1 and 10.")
+            option = int(1)
+            
+    while option == 2:
+        num = input ("Enter a whole number between 1 and 100")
+        num = int(num)
+        if num >=1 and num <= 100:
+            sum = repetition.sum_odd_numbers(num)
+            print(sum)
+            exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
+            exit = int(exit)
+            if exit == 1:
+                exit = 0
+                option = 0
+                quit = True
+            elif exit == 2:
+                exit = 0
+                option = 0
+        elif num <1 or num > 100:
+            print("(num) is not a valid number. Please enter a whole number between 1 and 100")
+            option = int(2)
+            
+    while option == 3:
+        exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No")
+        exit = int(exit)
+        if exit == 1:
+            exit = 0
+            option = 0
+            quit = True
+        elif exit == 2:
+            exit = 0
+            option = 0
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,22 @@
-#
+# Homework 3
+
+def get_factorial(num):
+    factorial = 1
+    for n in range(1, num+1):
+        factorial = factorial * n
+    return factorial
+
+def sum_odd_numbers(num):
+    sum = 0
+    n = 0
+    while n < num+1:
+        if n % 2 == 0:
+            n += 1
+        else:
+            sum = sum + n
+            n += 1
+    return sum
+
+
+
+
