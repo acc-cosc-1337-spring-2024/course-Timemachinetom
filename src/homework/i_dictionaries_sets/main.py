7cd30f875b86d086a00dbfa0d0e6fdd8fde20f47
Timemachinetom <108629469+Timemachinetom@users.noreply.github.com>
2024-03-30 18:17:14 +0000

    Homework 6

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,66 @@
-#
+import dictionary
+
+option = 0
+quit = False
+widgets = {}
+
+while quit == False:
+    option = input("Homework 6 Menu\n1-Add or Update Item\n2-Delete Item\n3-Exit")
+    option = int(option)
+
+    while option == 1:
+        widget_name = input("Enter item name")
+        quantity = input ("Enter item quantity. Enter negative to subtract.")
+        quantity = int(quantity)
+        dictionary.add_inventory(widgets, widget_name, quantity)
+        print (widgets) #used to check the dictionary after each menu choice
+        exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
+        exit = int(exit)
+        if exit == 1:
+            exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No")
+            exit = int(exit)
+            if exit == 1:
+                exit = 0
+                option = 0
+                print("Goodbye")
+                quit = True
+            else:
+                exit = 0
+                option = 0
+        else:
+            exit = 0
+            option = 0
+
+    while option == 2:
+        widget_name = input("Enter item name")
+        delete = dictionary.remove_inventory_widget (widgets, widget_name)
+        print (delete)
+        print (widgets)
+        exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
+        exit = int(exit)
+        if exit == 1:
+            exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No")
+            exit = int(exit)
+            if exit == 1:
+                exit = 0
+                option = 0
+                print("Goodbye")
+                quit = True
+            else:
+                exit = 0
+                option = 0
+        else:
+            exit = 0
+            option = 0
+
+    while option == 3:
+        exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No")
+        exit = int(exit)
+        if exit == 1:
+            exit = 0
+            option = 0
+            print("Goodbye")
+            quit = True
+        else:
+            exit = 0
+            option = 0
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,20 @@
-#
+import unittest
+#from src.homework.i_dictionaries_sets import get_p_distance
+
+class Test_Config(unittest.TestCase):
+    def test_add_inventory(self):
+        inventory_dictionary = {}
+        inventory_dictionary['Widget1'] = 10
+        self.assertEqual (inventory_dictionary['Widget1'], 10)
+        inventory_dictionary['Widget1'] += 25
+        self.assertEqual (inventory_dictionary['Widget1'], 35)
+        inventory_dictionary['Widget1'] += -10
+        self.assertEqual (inventory_dictionary['Widget1'], 25)
+    
+    def remove_inventory_widget(self):
+        inventory_dictionary = {}
+        inventory_dictionary['widget1'] = 10
+        inventory_dictionary['widget2'] = 25
+        del inventory_dictionary['widget1']
+        length = len(inventory_dictionary)
+        self.assertEqual (length, 1)
