5da3d047e79828d8fb8bfe0258fbd0d4394aa413
Timemachinetom <108629469+Timemachinetom@users.noreply.github.com>
2024-04-07 21:14:01 +0000

    Homework 7

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,19 +1,18 @@
-import dictionary
+import sets
 
 option = 0
 quit = False
 widgets = {}
+prog1 = set ({'Student1', 'Student2', 'Student3'})
+prog2=  set ({'Student3', 'Student4', 'Student5'})
 
 while quit == False:
-    option = input("Homework 6 Menu\n1-Add or Update Item\n2-Delete Item\n3-Exit")
+    option = input("Homework 7 Menu\n1-Students who took prog1 and prog2\n2-Students who took prog1 or prog2\n3-Students who took prog1 not prog2\n4-Students who took prog2 not prog1\n5-Exit")
     option = int(option)
 
     while option == 1:
-        widget_name = input("Enter item name")
-        quantity = input ("Enter item quantity. Enter negative to subtract.")
-        quantity = int(quantity)
-        dictionary.add_inventory(widgets, widget_name, quantity)
-        #print (widgets) #used to check the dictionary after each menu choice
+        students = sets.get_students_who_took_prog1_and_prog2 (prog1, prog2)
+        print (students)
         exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
         exit = int(exit)
         if exit == 1:
@@ -32,10 +31,8 @@ while quit == False:
             option = 0
 
     while option == 2:
-        widget_name = input("Enter item name")
-        delete = dictionary.remove_inventory_widget (widgets, widget_name)
-        print (delete)
-        #print (widgets)
+        students = sets.get_students_who_took_prog1_or_prog2(prog1, prog2)
+        print (students)
         exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
         exit = int(exit)
         if exit == 1:
@@ -54,6 +51,46 @@ while quit == False:
             option = 0
 
     while option == 3:
+        students = sets.get_students_who_took_prog1_not_prog_2(prog1,prog2)
+        print (students)
+        exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
+        exit = int(exit)
+        if exit == 1:
+            exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No")
+            exit = int(exit)
+            if exit == 1:
+                exit = 0
+                option = 0
+                print("Goodbye")
+                quit = True
+            else:
+                exit = 0
+                option = 0
+        else:
+            exit = 0
+            option = 0
+
+    while option == 4:
+        students = sets.get_students_who_took_prog2_not_prog_1(prog1,prog2)
+        print (students)
+        exit = input ("Do you want to exit>\n1 - Yes\n2 - no")
+        exit = int(exit)
+        if exit == 1:
+            exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No")
+            exit = int(exit)
+            if exit == 1:
+                exit = 0
+                option = 0
+                print("Goodbye")
+                quit = True
+            else:
+                exit = 0
+                option = 0
+        else:
+            exit = 0
+            option = 0
+
+    while option == 5:
         exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No")
         exit = int(exit)
         if exit == 1:
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,6 +1,7 @@
 import unittest
-#from src.homework.i_dictionaries_sets import get_p_distance
-
+from src.homework.i_dictionaries_sets.sets import get_students_who_took_prog1_and_prog2, get_students_who_took_prog1_or_prog2, get_students_who_took_prog1_not_prog_2, get_students_who_took_prog2_not_prog_1
+prog1 = set ({'Student1', 'Student2', 'Student3'})
+prog2=  set ({'Student3', 'Student4', 'Student5'})
 class Test_Config(unittest.TestCase):
     def test_add_inventory(self):
         inventory_dictionary = {}
@@ -18,3 +19,16 @@ class Test_Config(unittest.TestCase):
         del inventory_dictionary['widget1']
         length = len(inventory_dictionary)
         self.assertEqual (length, 1)
+
+
+    def test_get_students_who_took_prog1_and_prog2(self):
+        self.assertEqual ({'Student3'}, get_students_who_took_prog1_and_prog2(prog1, prog2))
+
+    def test_get_students_who_took_prog1_or_prog2(self):
+        self.assertEqual ({'Student1', 'Student2','Student3', 'Student4', 'Student5'}, get_students_who_took_prog1_or_prog2(prog1, prog2))
+
+    def test_get_students_who_took_prog1_not_prog_2(self):
+        self.assertEqual ({'Student1', 'Student2'}, get_students_who_took_prog1_not_prog_2(prog1, prog2))
+
+    def test_get_students_who_took_prog2_not_prog_1(self):
+        self.assertEqual ({'Student4', 'Student5'}, get_students_who_took_prog2_not_prog_1(prog1, prog2))
